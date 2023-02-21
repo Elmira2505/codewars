1d1c64e0b9cbefe523071621ea772f31f070394a
Elmira2505 <uan2505@mail.ru>
2023-02-21 03:51:30 -0500

    initial commit

---  Dan's great power generator/danspower.js
+++  Dan's great power generator/danspower.js
@@ -2,7 +2,4 @@ function danspower(num, power) {
     let result = Math.pow(num, power)
     console.log(num, power)
     return  num === 0 ? 0 : (result % 2 !== 0) ? Math.round(result /10) * 10 : result
-
-
-
 }
